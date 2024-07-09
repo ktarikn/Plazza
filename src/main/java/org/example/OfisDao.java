6e8acbcb0dec068921126a7afe25d9a229ceeaab
ktarikn <tarikn2003@gmail.com>
2024-07-09 17:12:33 +0300

    Added utilities

--- src/main/java/org/example/OfisDao.java
+++ src/main/java/org/example/OfisDao.java
@@ -23,4 +23,26 @@ public class OfisDao {
         return em.createQuery("from Ofis", Ofis.class).getResultList().toArray(new Ofis[0]);
     }
 
+    @Transactional
+    public Ofis findById(Long id) {
+        return em.find(Ofis.class, id);
+    }
+
+    @Transactional
+    public Ofis deleteById(Long Id) {
+        Ofis ofis = em.find(Ofis.class, Id);
+        em.remove(em.getReference(Ofis.class, Id));
+        return ofis;
+    }
+
+    @Transactional
+    public void delete(Ofis ofis) {
+        em.remove(ofis);
+    }
+
+    @Transactional
+    public Ofis update(Ofis ofis) {
+        return em.merge(ofis);
+    }
+
 }
